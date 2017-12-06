fb7aa2cb3b484154863cd88dd09f4f662507b874
Alicja Suszwedyk <asuszwedyk@gmail.com>
2017-12-06 15:24:32 +0100

    first version with working broadcast receiver and notifications

---
+++ SendMessageApp/app/src/main/java/com/example/alicja/sendmessageapp/MainActivity.java
@@ -0,0 +1,31 @@
+package com.example.alicja.sendmessageapp;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+
+    EditText messageToSend;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        messageToSend = (EditText) findViewById(R.id.editText);
+    }
+
+
+    public void sendMessage(View view) {
+        Intent messageIntent = new Intent();
+        messageIntent.setAction("com.example.alicja.sendmessageapp.MESSAGE_INTENT");
+
+        String messageText = messageToSend.getText().toString();
+        messageIntent.putExtra("messageText", messageText);
+
+        sendBroadcast(messageIntent);
+
+    }
+}
